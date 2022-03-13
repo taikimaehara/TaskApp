dc4df8fb7ec0252f9170aa93b6bc8a77580c3acc
ta-maehara <ta-maehara@kddi-tech.com>
2022-03-13 16:16:55 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/taiki/maehara/taskapp/Task.kt
@@ -0,0 +1,16 @@
+package jp.techacademy.taiki.maehara.taskapp
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import java.io.Serializable
+import java.util.Date
+
+open class Task : RealmObject(), Serializable {
+    var title: String = ""
+    var contents: String = ""
+    var date: Date = Date()
+
+    // idをプライマリーキーとして設定
+    @PrimaryKey
+    var id: Int = 0
+}
